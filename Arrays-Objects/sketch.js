dfd00ce7eb1d46c01cc04fc5333fb78411183bc8
JonahWJohnson <146984197+JonahWJohnson@users.noreply.github.com>
2023-10-10 12:17:17 -0600

    added jef object and attributes

--- Arrays-Objects/sketch.js
+++ Arrays-Objects/sketch.js
@@ -5,6 +5,19 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let jef = {
+  x: 200,
+  y: 200,
+  dx: 5,
+  dy: 5,
+  size: 50,
+  weight: 5,
+}
+
+let gravityProperties = {
+  strength: 5,
+
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -12,4 +25,9 @@ function setup() {
 
 function draw() {
   background(220);
+}
+
+
+function moveJef() {
+// base movement 
 }
