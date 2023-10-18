66dc8c17dd75de09aabc0f6c20789b30247a1e67
JonahWJohnson <146984197+JonahWJohnson@users.noreply.github.com>
2023-10-18 12:12:19 -0600

    Update sketch.js

--- Arrays-Objects/sketch.js
+++ Arrays-Objects/sketch.js
@@ -6,16 +6,17 @@
 // - describe what you did to take this project "above and beyond"
 
 let jef = {
-  x: 200,
-  y: 200,
+  x: 500,
+  y: 500,
   dx: 5,
   dy: 0,
   size: 50,
   weight: 5,
 }
-
+let isGrounded;
 let gravityProperties = {
-  strength: -5,
+  strength: 0.2,
+  acceleration: 0.1,
 
 }
 
@@ -25,7 +26,10 @@ function setup() {
 
 function draw() {
   background(220);
-  moveJef()
+  spawnJef();
+  moveJef();
+  collision();
+  centerJef()
 }
 
 
@@ -41,16 +45,49 @@ function moveJef() {
   }
   if (keyIsDown(68)) {
     //d
-    jef.x =+ jef.dx;
+    jef.x += jef.dx;
   }
   if (keyIsDown(16)) {
+    //shift
     boost()
   }
 //adds gravity and applies it
-jef.y += gravityProperties.strength;
+jef.dy += gravityProperties.strength;
 jef.y += jef.dy
+
+//
 }
 
 function spawnJef(){
   square(jef.x, jef.y, jef.size)
 }
+
+function collision() {
+  if (jef.dy === 0) {
+    isGrounded === true;
+  }
+  else {
+    isGrounded === false;
+  }
+
+  if (jef.y >= height + jef.size/2) {
+    jef.y = height - jef.size;
+  }
+}
+
+function jump() {
+  // Checks if character is grounded
+  if (jef.y + jef.size === height) {
+    //adds jumpStrength to vertical velocitty
+    jef.dy = jumpStrength;
+  }
+}
+
+function centerJef() {
+  if (keyIsDown(67)) {
+    jef.x = width/2;
+    jef.y = height/2;
+    jef.dy = 0
+    jef.dx = 0
+  }
+}c
